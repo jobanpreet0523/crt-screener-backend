b96ebb7b253427c1bf68821eb9c0738db41855aa
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-15 17:02:34 +0530

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,6 +1,7 @@
-import pandas as pd
-
 def get_us_stocks():
-    url = "https://datahub.io/core/nasdaq-listings/r/nasdaq-listed-symbols.csv"
-    df = pd.read_csv(url)
-    return df["Symbol"].tolist()
+    return [
+        "AAPL", "MSFT", "TSLA",
+        "NVDA", "AMZN", "META",
+        "GOOGL", "NFLX"
+    ]
+
