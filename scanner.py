8d76924478210164e4d9d8257826680b015324d0
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-16 15:11:59 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,9 +1,6 @@
-from crt_logic import classify_crt
-
 def run_crt_scan(tf="daily"):
-    # Placeholder (you will add real data later)
     return {
         "timeframe": tf,
-        "message": "Scanner running successfully",
+        "message": "CRT scanner running",
         "results": []
     }
