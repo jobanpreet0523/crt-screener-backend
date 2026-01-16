d005d0edcfc3025b70d4823ce3d1fdf8ebcdb879
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-17 00:25:00 +0530

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,9 +1,10 @@
-FOREX_PAIRS = [
-    "EURUSD=X",
-    "GBPUSD=X",
-    "USDJPY=X",
-    "AUDUSD=X",
-    "USDCHF=X",
-    "USDCAD=X",
-    "NZDUSD=X"
-]
+def get_forex_pairs():
+    return [
+        "EURUSD=X",
+        "GBPUSD=X",
+        "USDJPY=X",
+        "AUDUSD=X",
+        "USDCHF=X",
+        "USDCAD=X",
+        "NZDUSD=X"
+    ]
