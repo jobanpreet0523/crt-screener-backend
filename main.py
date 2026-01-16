6d1b9bd123d53547865b54278671de2cfa68052b
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-16 14:03:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI, Query
-from scanner import run_crt_scan
+from app.scanner import run_crt_scan
 
 app = FastAPI(
     title="CRT Screener Backend",
     version="1.0.0"
 )
 
-
 @app.get("/")
-def health_check():
+def root():
     return {"status": "CRT Backend Running"}
-
 
 @app.get("/scan")
 def scan(tf: str = Query(default="daily")):
