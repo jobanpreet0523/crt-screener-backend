1807d1b70a9b1e477d63a3e1b802e42247256efd
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-19 00:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from batch_scan import run_nse200_scan
 
-app = FastAPI()
+app = FastAPI(title="CRT Screener Backend")
 
 @app.get("/")
 def health():
     return {"status": "ok"}
 
 @app.get("/scan/nse200")
-def scan_nse_200():
+def scan_nse200():
+    results = run_nse200_scan()
     return {
-        "count": len(run_nse200_scan()),
-        "results": run_nse200_scan()
+        "count": len(results),
+        "results": results
     }
