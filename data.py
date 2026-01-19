7c99f1c8b9b52b817efe3289315b4b7337faefb8
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-19 21:14:03 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,14 +1,6 @@
 import yfinance as yf
 
 def get_ohlc(symbol):
-    ticker = yf.Ticker(symbol + ".NS")
-    df = ticker.history(period="5d")
-    if df.empty:
-        return None
-    last = df.iloc[-1]
-    return {
-        "open": round(last["Open"], 2),
-        "high": round(last["High"], 2),
-        "low": round(last["Low"], 2),
-        "close": round(last["Close"], 2)
-    }
+    ticker = yf.Ticker(f"{symbol}.NS")
+    df = ticker.history(period="5d", interval="1d")
+    return df
