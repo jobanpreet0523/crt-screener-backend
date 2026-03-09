a27da62de5f6e968a80f7fc1a726a5a4e181acd1
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-03-09 11:31:47 +0530

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -1,7 +1,20 @@
-TICKERS = [
-"AAPL","MSFT","TSLA","NVDA","AMZN",
-"GOOGL","META","NFLX","AMD","INTC",
-"IBM","ORCL","SHOP","PYPL","UBER",
-"DIS","BA","GE","KO","PEP",
-# add thousands more
-]
+import pandas as pd
+
+def get_all_tickers():
+
+    # NASDAQ stocks
+    nasdaq = pd.read_csv(
+        "https://raw.githubusercontent.com/datasets/nasdaq-listings/master/data/nasdaq-listed.csv"
+    )
+
+    # NYSE stocks
+    nyse = pd.read_csv(
+        "https://raw.githubusercontent.com/datasets/nyse-listed/master/data/nyse-listed.csv"
+    )
+
+    nasdaq_tickers = nasdaq["Symbol"].tolist()
+    nyse_tickers = nyse["ACT Symbol"].tolist()
+
+    tickers = list(set(nasdaq_tickers + nyse_tickers))
+
+    return tickers
